411f87f49d0b13e048d05de1f8386473bfc1c908
Arut <arutyun-1996@yandex.ru>
2022-06-16 15:49:12 +0300

    initv5

--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,6 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const {User} = require('../db/models')
 
-router.get('/', (req, res) => {
-  res.render('login');
-});
-
 router.post('/', async (req, res) => {
   const {email, password} = req.body
   if (email && password) { 
